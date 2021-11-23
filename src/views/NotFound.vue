f008b7911a2a18a7590de77211b551fe98fececc
tongZhezhe <9807671+tongzhezhe@user.noreply.gitee.com>
2021-11-23 16:13:53 +0800

    添加了404页面

---
+++ src/views/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>当前页面不存在，请检查网络是否正确</h2>
+    <router-link to="/">返回主页</router-link>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "NotFound"
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
